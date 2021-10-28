affc4d8f2883c8232a3b6dd956695004d1b486a0
Gautham Ganesh <68129413+gsgautham98@users.noreply.github.com>
2021-10-28 02:17:50 +0200

    2nd version of code & report for t-test practical

--- code2.r
+++ code2.r
@@ -56,7 +56,7 @@ lines(delta.list, rpowers.poisson, col = "green")
 lines(delta.list, rpowers.normal, col = "red")
 legend(x = "topleft", legend = c("Theoretical", "Poisson", "Normal"), cex = 0.8, lty = c(1, 1, 1), col = c("blue", "green", "red"), bty = "n")
 
-# Task 5
+# Task 5, 9
 for (sample.size in c(10, 100, 1000)) {
   v.equal.power <- c()
   v.unequal.power <- c()
@@ -72,8 +72,9 @@ for (sample.size in c(10, 100, 1000)) {
     v.equal.power <- append(v.equal.power, sum(pvalues.equal < 0.05) / 1000)
     v.unequal.power <- append(v.unequal.power, sum(pvalues.unequal < 0.05) / 1000)
   }
-  plot(delta.list, v.equal.power, type = "l", col = "blue", main = paste0("Plot with sample size ", sample.size))
+  plot(delta.list, v.equal.power, type = "l", col = "blue", xlab = "delta", ylab = "Power", main = paste0("Plot with sample size ", sample.size))
   points(delta.list, v.unequal.power, col = "red", pch = 20, cex = 0.5)
+  legend(x = "bottomright", legend = c("Equal variances t-test", "Unequal variances t-test"), cex = 0.8, lty = c(1, 2), col = c("blue", "red"), bty = "n")
 }
 
 # Task 6
@@ -91,3 +92,20 @@ f.pos.uneq.rate <- f.pos.uneq.list / seq_along(v.seq)
 plot(v.seq, f.pos.eq.rate, type = "l", col = "blue", xlab = "Variance", ylab = "False positive rate", main = "Evolution of false positive rate for a normally-distributed sample")
 lines(v.seq, f.pos.uneq.rate, col = "red")
 legend(x = "topright", c("Equal variances t-test", "Unequal variances t-test"), cex = 0.8, lty = c(1, 1), col = c("blue", "red"), bty = "n")
+
+# Task 7, 8, 9
+n.list <- seq(100, 5100, 250)
+t.powers.normal <- c()
+w.powers.normal <- c()
+t.powers.poisson <- c()
+w.powers.poisson <- c()
+for (n in n.list) {
+  t.powers.normal <- append(t.powers.normal, mean(replicate(1000, t.test(rnorm(n), rnorm(n), var.equal = TRUE)$p.value) < 0.05))
+  w.powers.normal <- append(w.powers.normal, mean(replicate(1000, wilcox.test(rnorm(n), rnorm(n))$p.value) < 0.05))
+  t.powers.poisson <- append(t.powers.poisson, mean(replicate(1000, t.test(rpois(n, 1), rpois(n, 1))$p.value) < 0.05))
+  w.powers.poisson <- append(w.powers.poisson, mean(replicate(1000, wilcox.test(rpois(n, 1), rpois(n, 1))$p.value) < 0.05))
+}
+plot(n.list, t.powers.normal, main = "Comparison of t-test and Mann-Whitney U test - normal dist", xlab = "Sample size", ylab = "Power", col = "blue", type = "l")
+lines(n.list, w.powers.normal)
+plot(n.list, t.powers.poisson, main = "Comparison of t-test and Mann-Whitney U test - Poisson dist", xlab = "Sample size", ylab = "Power", col = "blue", type = "l")
+lines(n.list, w.powers.poisson)
